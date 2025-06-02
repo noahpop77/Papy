752ecb9c4655d631b681adeddd23d09742605822
noahpop77 <noahpop77@gmail.com>
2025-06-02 13:12:24 -0700

    test

--- src/myRandom.cpp
+++ src/myRandom.cpp
@@ -21,7 +21,7 @@ std::string myRandom::generateRandomString(size_t length) {
     constexpr char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     constexpr size_t chars_len = sizeof(chars) - 1;
     
-    static std::uniform_int_distribution<> distrib(0, chars_len - 1);
+    std::uniform_int_distribution<> distrib(0, chars_len - 1);
 
     std::string randomStr;
     randomStr.reserve(length);
@@ -35,7 +35,7 @@ std::string myRandom::generateRandomNumberString(size_t length) {
     constexpr char chars[] = "0123456789";
     constexpr size_t chars_len = sizeof(chars) - 1;
     
-    static std::uniform_int_distribution<> distrib(0, chars_len - 1);
+    std::uniform_int_distribution<> distrib(0, chars_len - 1);
 
     std::string randomStr;
     randomStr.reserve(length);
@@ -46,7 +46,7 @@ std::string myRandom::generateRandomNumberString(size_t length) {
 }
 
 bool myRandom::getRandomBool() {
-    static std::uniform_int_distribution<> distrib(0, 1);
+    std::uniform_int_distribution<> distrib(0, 1);
 
     // Return true if random number is 1, else false
     return distrib(gen) == 1;
